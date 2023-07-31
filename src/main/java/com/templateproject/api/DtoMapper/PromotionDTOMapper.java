605779afc4335292d1c870590c5abe3655df249a
HeleneB1 <helene.blanche@hotmail.fr>
2023-07-31 14:56:54 +0200

    Add last changes on promotion

--- src/main/java/com/templateproject/api/DtoMapper/PromotionDTOMapper.java
+++ src/main/java/com/templateproject/api/DtoMapper/PromotionDTOMapper.java
@@ -19,6 +19,8 @@ public class PromotionDTOMapper implements Function<Promotion, PromotionDTO> {
                 promotion.getDescription(),
                 promotion.getBanner(),
                 promotion.getPicture(),
+                promotion.getDifficulty(),
+                promotion.getType(),
                 promotion.getRating(),
                 promotion.getCreationDate(),
                 promotion.getTopics(),
